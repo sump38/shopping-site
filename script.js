e3a73975b2bcb5409f3b6938b41d9329ce501707
unknown <niravr94@gmail.com>
2022-11-17 17:10:58 +0200

    adding items to the main page using handlebars.

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
+// const { Handlebars } = require("./handlebars-v4.7.7");
+
 const productContainer = document.getElementById("products-container");
 const submitBtn = document.getElementById("subminBtn");
 const newItemField = document.getElementById("newItemField");
 
 
-fetch('http://localhost:3000/items')
+fetch('https://raw.githubusercontent.com/MrchinFTW/class-work/main/products.json')
     .then((response) => response.json())
     .then((data) =>{
-       
+       console.log(data);
         createNewItem(data)
     });
 
 function createNewItem(items){
-    for (let i = 0; i < items.length; i++) {
+    var rawTemplate = document.getElementById('itemsTemplate').innerHTML;
+    var compiledTemplate = Handlebars.compile(rawTemplate);
+    var generatedHTML = compiledTemplate(items)
+    var productContaner = document.getElementById('products-container');
+    productContainer.innerHTML = generatedHTML
+}
+
+
+
+
+
+/**
+     for (let i = 0; i < items.length; i++) {
         //create and modify the elements.
         const colDIV = document.createElement("div");
         colDIV.classList.add("col");
@@ -35,8 +49,7 @@ function createNewItem(items){
         kgSmall.innerText = "$/kg"
         const itemDescription = document.createElement("p");
         itemDescription.innerText = items[i].description;
-        
-            //append section
+            //********append section************
         productContainer.appendChild(colDIV)
         colDIV.appendChild(cardDIV)
         cardDIV.appendChild(cardHeaderDIV)
@@ -47,4 +60,5 @@ function createNewItem(items){
         cardBody.appendChild(itemDescription)
         priceTitle.appendChild(kgSmall);
     }
-}
+  
+ */
